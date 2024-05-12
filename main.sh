17ad96f3acf47cc2d71b60deab3a7b6ea598a63e
1khazipov <aaooo00aa0@gmail.com>
2024-05-12 23:54:42 +0300

    added main.sh

---
+++ main.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Run the big data pipeline
+echo "Running Stage 1 of the pipeline - PostgreSQL + Sqoop"
+bash scripts/stage1.sh
+
+echo "Running Stage 2 of the pipeline - Hive + Spark SQL"
+bash scripts/stage2.sh
+
+echo "Running Stage 3 of the pipeline - Spark ML"
+bash scripts/stage3.sh
+
+echo "Running Stage 4 of the pipeline - Streamlit"
+bash scripts/stage4.sh
+
+# Check the quality of the codes
+echo "The quality of scripts in 'scripts/' folder\n"
+echo "::============================================::"
+pylint scripts
+
+
+echo "Done testing the pipeline!"
